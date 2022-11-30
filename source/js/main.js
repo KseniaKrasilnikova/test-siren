00cc9fcada46ffb46085ea57c6d343761827364c
KseniaKrasilnikova <tksenia13@gmail.com>
2022-11-30 20:37:25 +0200

    init commit

---
+++ source/js/main.js
@@ -0,0 +1,17 @@
+import {initBurger} from './modules/init-burger-action';
+
+// ---------------------------------
+
+window.addEventListener('DOMContentLoaded', () => {
+
+  // Utils
+  // ---------------------------------
+
+  // Modules
+  // ---------------------------------
+  window.addEventListener('load', () => {
+    initBurger();
+  });
+});
+
+// ---------------------------------
